d5c59f89660cf0da1d502f49d342c52104fd2d8c
nbglink@gmail.com <nbglink@gmail.com>
2015-03-20 23:43:05 +0200

    HW8-Advanced Topics

---
+++ HW8-Advanced Topics/S1SortingNumbers/SortingNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace S1SortingNumbers
+{
+    class SortingNumbers
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Enter your numbers count: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbersCount = new int[n];
+            Console.WriteLine("Enter your numbers in separete lines: ");
+            for (int i = 0; i < numbersCount.Length; i++)
+            {
+                numbersCount[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbersCount);
+            Console.WriteLine("Your result sequance is: ");
+            foreach (var number in numbersCount)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
